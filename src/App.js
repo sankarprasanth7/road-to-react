92b77adcec313e03d7e17d60743452b478655768
sankara prasanth <prasanth@inflolabs.com>
2019-11-02 13:51:54 +0530

    integrated hot module reload and basics

--- src/App.js
+++ src/App.js
@@ -1,26 +1,38 @@
-import React from 'react';
+import React, {Component} from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
+const list = [
+  {
+  title: 'React',
+  url: 'https://reactjs.org/',
+  author: 'Jordan Walke',
+  num_comments: 3,
+  points: 4,
+  objectID: 0,
+  },
+  {
+  title: 'Redux',
+  url: 'https://redux.js.org/',
+  author: 'Dan Abramov, Andrew Clark',
+  num_comments: 2,
+  points: 5,
+  objectID: 1,
+  },
+];
+
+class App extends Component {
+  render() {
+    var welcomeMessage = "Welcome to road of react";
+    return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    {list.map(function(item){
+      return <div>{item.title}</div>;
+    })}
+      <h1> {welcomeMessage}</h1>
     </div>
-  );
+    );
+  }
 }
 
 export default App;
